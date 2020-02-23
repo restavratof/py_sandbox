717f8a7d75fb44a64a33af3f50a8aabc55127c3f
dfashch <d.faschenko@gmail.com>
2020-02-24 01:08:27 +0300

    openweather api changes

--- api/openweathermap/openweathermap-client.py
+++ api/openweathermap/openweathermap-client.py
@@ -1,32 +1,46 @@
 import requests
 import json
 import os
-
-# API_KEY = os.environ['OPENWEATHERMAP_API_KEY'] or 'you-will-never-guess'
-
-print(f"OS ENV: {os.environ}")
-
-for k, v in os.environ.items():
-    print(f'{k}={v}')
-
-print(type(os.environ))
+import configparser
 
 
-print(f"API_KEY: {API_KEY}")
+config = configparser.ConfigParser()
+config.sections()
+config.read('env.properties')
+
+API_KEY = config['DEFAULT']['API_KEY']
+
+print(API_KEY)
+
+# URL = 'api.openweathermap.org/data/2.5/forecast?id=524901&APPID=1111111111'
+
+#    "id": 627907,
+#     "name": "Homyel",
+#     "country": "BY",
+#     "coord": {
+#       "lon": 30.975401,
+#       "lat": 52.434502
+#     }
+city_id = 627907
+
+# Minski Rayon
+city_id = 625140
+
+
+URL = f"http://api.openweathermap.org/data/2.5/forecast?id={city_id}&APPID={API_KEY}"
+
+DATA = {
+    "id":city_id,
+    "APPID":API_KEY
+}
+
+# r = requests.post(url = URL, data=json.dumps(DATA))
+r = requests.post(url = URL)
 
 
 
-
-
-# MSG = '*Hello* - This is a test message from Dima. Ignore that, please.'
-# DATA = {"text":MSG}
-#
-# r = requests.post(url = WEBHOOK_URL, data=json.dumps(DATA))
-#
-#
-#
-# print('-'*50)
-# # extracting response text
-# pastebin_url = r.text
-# print("The pastebin URL is:%s"%pastebin_url)
+print('-'*50)
+# extracting response text
+pastebin_url = r.text
+print("The pastebin URL is:%s"%pastebin_url)
 
