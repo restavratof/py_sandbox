b248b85a04da9f2d479c56fa4c9a7c2ee0e11f13
restavratof <d.faschenko@gmail.com>
2020-02-24 18:28:16 +0300

    event loop initial

--- async/1_select.py
+++ async/1_select.py
@@ -1,5 +1,7 @@
 import socket
+from select import select
 
+to_monitor = []
 
 srv_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 srv_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -8,23 +10,30 @@ srv_socket.listen()
 
 
 def accept_connection(server_socket):
-    while True:
-        client_socket, addr = server_socket.accept()
-        print('Connection from', addr)
-        send_message(client_socket)
+    client_socket, addr = server_socket.accept()
+    print('Connection from', addr)
+    to_monitor.append(client_socket)
 
 def send_message(client_socket):
+    request = client_socket.recv(4096)
+
+    if request:
+        response = 'Hello World\n'.encode()
+        client_socket.send(response)
+    else:
+        client_socket.close()
+
+def event_loop():
     while True:
-        request = client_socket.recv(4096)
+        ready_to_read, _, _ = select(to_monitor, [], [])  # read, write, errors
 
-        if not request:
-            break
-        else:
-            response = 'Hello World\n'.encode()
-            client_socket.send(response)
-
-    client_socket.close()
-
+        for sock in ready_to_read:
+            if sock is srv_socket:
+                accept_connection(srv_socket)
+            else:
+                send_message(sock)
 
 if __name__ == '__main__':
-    accept_connection(srv_socket)
+    to_monitor.append(srv_socket)
+    # accept_connection(srv_socket)
+    event_loop()
