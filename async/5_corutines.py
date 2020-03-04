11b6ec6e1308e6204caa2992974167a157531f87
restavratof <d.faschenko@gmail.com>
2020-03-04 13:55:39 +0300

    del init

--- async/5_corutines.py
+++ async/5_corutines.py
@@ -1,5 +1,33 @@
 
+def coroutine(func):
+    def inner(*args, **kwargs):
+        g = func(*args, **kwargs)
+        g.send(None)
+        return g
+    return inner
+
 def subgen():
     x = 'Ready'
     msg = yield x
     print('Subgen received:', msg)
+
+class BlaBlaException(Exception):
+    pass
+
+@coroutine
+def average():
+    count = 0
+    summ = 0
+    average = None
+
+    while True:
+        try:
+            x = yield average
+        except StopIteration:
+            print('End')
+        except BlaBlaException:
+            print('--------------------')
+        else:
+            count += 1
+            summ += x
+            average = round(summ/count, 2)
---
+++ async/6_del.py
@@ -0,0 +1,50 @@
+
+def coroutine(func):
+    def inner(*args, **kwargs):
+        g = func(*args, **kwargs)
+        g.send(None)
+        return g
+    return inner
+
+
+class BlaBlaException(Exception):
+    pass
+
+# @coroutine
+def subgen():
+    while True:
+        try:
+            msg = yield
+        except StopIteration:
+            # print('Bla Bla')
+            break
+        else:
+            print('......', msg)
+    return 'STR1'
+
+@coroutine
+def delegator(g):
+    # while True:
+    #     try:
+    #         data = yield
+    #         g.send(data)
+    #     except BlaBlaException as e:
+    #         g.throw(e)
+    result = yield from g
+    print(result)
+
+
+def fib(n):
+    if n < 2:
+        return 1
+    else:
+        return fib(n-1) + fib(n-2)
+
+print(fib(1))
+print(fib(2))
+print(fib(3))
+print(fib(4))
+print(fib(5))
+print(fib(6))
+print(fib(7))
+print(fib(8))
