16a965ae4b4146fced8316b51588240a0b90eb17
restavratof <d.faschenko@gmail.com>
2020-06-04 14:11:01 +0300

    add iso format example

--- date/dateutil_example.py
+++ date/dateutil_example.py
@@ -1,4 +1,5 @@
 from dateutil import parser
+from datetime import datetime
 
 str1 = '2019-08-28T14:34:25.518993Z'
 str2 = '2020-04-09T03:31:37.846Z'
@@ -9,3 +10,6 @@ print(type(date1))
 date2 = parser.parse(str2)
 print(date2)
 print(type(date2))
+
+print('-'*50)
+print(f'ISO: {datetime.now().isoformat()}')
