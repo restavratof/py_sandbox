2ac5fb51f62bba6ddbf37d81ab760fb3b8742dc6
restavratof <d.faschenko@gmail.com>
2020-06-18 15:07:20 +0300

    add pydoc sample

---
+++ core/doc.py
@@ -0,0 +1,11 @@
+
+
+def add(a,b):
+    """This function adds two numbers"""
+    return a+b
+
+
+sum = add(1,3)
+print("Access docstring method 1:", add.__doc__)
+print("Access docstring method 2:", end="")
+help(add)
