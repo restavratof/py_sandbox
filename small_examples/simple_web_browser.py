a270ac329c60e819ccd84d8f051ae0c5d1f4c82e
dfashchanka <dfashchanka@gomel.iba.by>
2021-12-28 22:20:45 +0300

    simple web browser minor updates

--- small_examples/simple_web_browser.py
+++ small_examples/simple_web_browser.py
@@ -1,8 +1,15 @@
 import socket
 
+# host = 'data.pr4e.org'
+host = 'localhost'
+# port = 80
+port = 9000
+
+
 mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-mysock.connect(('data.pr4e.org', 80))
-cmd = 'GET http://data.pr4e.org/page1.htm HTTP/1.0\r\n\r\n'.encode()
+mysock.connect((host, port))
+# cmd = 'GET http://data.pr4e.org/page1.htm HTTP/1.0\r\n\r\n'.encode()
+cmd = 'GET http://localhost:9000 HTTP/1.1\r\n\r\n'.encode()
 mysock.send(cmd)
 
 while True:
