3809c998068322f1265ebadf315cb2387a54d424
dfashchanka <dfashchanka@gomel.iba.by>
2022-03-02 18:57:20 +0300

    built-in constants

---
+++ python_standard_library/1_built-in/built-in_constants.py
@@ -0,0 +1,35 @@
+# False - The false value of the bool type. Assignments to False are illegal and raise a SyntaxError.
+#
+# True - The true value of the bool type. Assignments to False are illegal and raise a SyntaxError.
+#
+# None - None is the sole instance of the NoneType type. An object frequently used to represent the absence of a value,
+#       as when default arguments are not passed to a function. Assignments to None are illegal and raise a SyntaxError.
+#
+# NotImplemented -
+#
+# Ellipsis - The same as the ellipsis literal “...”.
+#           Special value used mostly in conjunction with extended slicing syntax for user-defined container data types.
+#           Ellipsis is the sole instance of the types.EllipsisType type.
+#
+# __debug__ - This constant is true if Python was not started with an -O option
+#
+# Constants added by the site module:
+# a) quit(code=None)
+#    exit(code=None)
+#      Objects that when printed, print a message like “Use quit() or Ctrl-D (i.e. EOF) to exit”, and when called,
+#      raise SystemExit with the specified exit code.
+# b) copyright
+#    credits
+#      Objects that when printed or called, print the text of copyright or credits, respectively.
+# c) license
+#      Object that when printed, prints the message “Type license() to see the full license text”,
+#      and when called, displays the full license text in a pager-like fashion (one screen at a time).
+
+
+
+
+
+
+
+
+
---
+++ python_standard_library/1_built-in/built-in_functions.py
@@ -0,0 +1,1 @@
+# abs(x)  - Return the absolute value of a number. (argument: integer, float, object implementing __abs__())
